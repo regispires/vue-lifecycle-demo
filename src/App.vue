e9127a49e1d4b67e60ed181e8edd9aece9af3b5a
Regis Pires Magalhães <regispiresmag@gmail.com>
2025-05-13 21:30:58 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <button @click="show = !show">
+    {{ show ? 'Ocultar' : 'Mostrar' }} Componente
+  </button>
+  <LifecycleDemo v-if="show" />
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import LifecycleDemo from './components/LifecycleDemo.vue'
+
+const show = ref(true)
+</script>
---
+++ src/components/LifecycleDemo.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <p>Contador: {{ state.count }}</p>
+    <p>Dobro: {{ doubleCount }}</p>
+    <button @click="increment">Incrementar</button>
+  </div>
+</template>
+
+<script setup>
+import {
+  reactive,
+  computed,
+  onBeforeMount,
+  onMounted,
+  onBeforeUpdate,
+  onUpdated,
+  onBeforeUnmount,
+  onUnmounted
+} from 'vue'
+
+console.log('setup() - componente está sendo criado')
+
+const state = reactive({
+  count: 0
+})
+
+const doubleCount = computed(() => {
+  console.log('computed: doubleCount reavaliado')
+  return state.count * 2
+})
+
+const increment = () => {
+  state.count++
+}
+
+onBeforeMount(() => {
+  console.log('onBeforeMount - antes de montar no DOM')
+})
+
+onMounted(() => {
+  console.log('onMounted - componente montado no DOM')
+})
+
+onBeforeUpdate(() => {
+  console.log('onBeforeUpdate - antes da atualização do DOM')
+})
+
+onUpdated(() => {
+  console.log('onUpdated - após atualização do DOM')
+})
+
+onBeforeUnmount(() => {
+  console.log('onBeforeUnmount - antes do componente ser desmontado')
+})
+
+onUnmounted(() => {
+  console.log('onUnmounted - componente desmontado')
+})
+</script>
